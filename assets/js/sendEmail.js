5a0ee3b45f0428848437045c1d40151ec58e73b0
Shaun Lindsley <shaunlindsley@gmail.com>
2020-07-16 15:33:05 +0000

    Added cdocumentation comments to sendEmail.js

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,11 @@
+// Display alert for form submit button
 $(document).ready(function() {
     $("form").submit(function() {
         alert("Thank you, the form has been submitted!");
     });
 });
 
+// Function to send email information to email.js
 function sendEmail(contactForm) {
     emailjs.send("gmail","recipeasy", {
         "from_name": contactForm.name.value,
