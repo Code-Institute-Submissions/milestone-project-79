3448f04b731e5bf2fc820644841a47c55a014f65
Shaun Lindsley <shaunlindsley@gmail.com>
2020-07-16 15:19:43 +0000

    Added feature: submit alert upon contact form submission

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+    $("form").submit(function() {
+        alert("Thank you, the form has been submitted!");
+    });
+});
+
 function sendEmail(contactForm) {
     emailjs.send("gmail","recipeasy", {
         "from_name": contactForm.name.value,
